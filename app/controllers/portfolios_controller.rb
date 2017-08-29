d5ce36bfc1c8720a840ab256f164b04f6e71a623
Oshni <oznai01@gmail.com>
2017-08-29 17:17:39 +0800

    Fix landing page

---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,25 @@
+class PortfoliosController < ApplicationController
+
+
+  def index 
+
+  end
+
+  def sketch
+
+  end
+
+  def painting
+
+  end
+
+  def digital_art
+
+  end
+
+  def web_design
+
+  end
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :portfolios
+
+  get 'sketch', to: 'portfolios#sketch', as: :sketch
+  get 'painting', to: 'portfolios#painting', as: :painting
+  get 'digital_art', to: 'portfolios#digital_art', as: :digital_art
+  get 'web_design', to: 'portfolios#web_design', as: :web_design
+
+  root "portfolios#index"
+
+end
